d26e1534e24492db2cb008d71b4bf9568caed390
Frank van Wijk <fvanwijk@xebia.com>
2018-09-10 23:17:31 +0200

    Initial repo with Semantic UI CSS and one button that plays a cool rhythm based on 3 loaded sounds

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var RhythmSample = function() {
+  loadSounds(this, {
+    kick: './sounds/kick.wav',
+    snare: './sounds/snare.wav',
+    hihat: './sounds/hihat.wav'
+  });
+};
+
+RhythmSample.prototype.play = function() {
+  // We'll start playing the rhythm 100 milliseconds from "now"
+  var startTime = context.currentTime + 0.100;
+  var tempo = 80; // BPM (beats per minute)
+  var eighthNoteTime = (60 / tempo) / 2;
+
+  // Play 2 bars of the following:
+  for (var bar = 0; bar < 2; bar++) {
+    var time = startTime + bar * 8 * eighthNoteTime;
+    // Play the bass (kick) drum on beats 1, 5
+    playSound(this.kick, time);
+    playSound(this.kick, time + 4 * eighthNoteTime);
+
+    // Play the snare drum on beats 3, 7
+    playSound(this.snare, time + 2 * eighthNoteTime);
+    playSound(this.snare, time + 6 * eighthNoteTime);
+
+    // Play the hi-hat every eighthh note.
+    for (var i = 0; i < 8; ++i) {
+      playSound(this.hihat, time + i * eighthNoteTime);
+    }
+  }
+};
